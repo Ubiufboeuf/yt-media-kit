d7226160738c7ba050a96975b69fd4dd08ad24e7
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-08-19 16:39:05 -0300

    feat: crear constantes

---
+++ src/lib/constants.ts
@@ -0,0 +1,112 @@
+export const COLORS = {
+	system: 'system',
+	black: 'black',
+	red: 'red',
+	green: 'green',
+	yellow: 'yellow',
+	blue: 'blue',
+	magenta: 'magenta',
+	cyan: 'cyan',
+	white: 'white',
+	gray: 'gray',
+	redbright: 'redBright',
+	greenbright: 'greenBright',
+	yellowbright: 'yellowBright',
+	bluebright: 'blueBright',
+	magentabright: 'magentaBright',
+	cyanbright: 'cyanBright',
+	whitebright: 'whiteBright'
+}
+
+export const BGCOLORS = {
+	transparent: 'transparent',
+	black: 'black',
+	red: 'red',
+	green: 'green',
+	yellow: 'yellow',
+	blue: 'blue',
+	magenta: 'magenta',
+	cyan: 'cyan',
+	white: 'white',
+	blackbright: 'blackBright',
+	redbright: 'redBright',
+	greenbright: 'greenBright',
+	yellowbright: 'yellowBright',
+	bluebright: 'blueBright',
+	magentabright: 'magentaBright',
+	cyanbright: 'cyanBright',
+	whitebright: 'whiteBright'
+}
+
+export const GRADIENTCOLORS = {
+	transparent: 'transparent',
+	black: 'black',
+	red: 'red',
+	green: 'green',
+	yellow: 'yellow',
+	blue: 'blue',
+	magenta: 'magenta',
+	cyan: 'cyan',
+	white: 'white',
+	gray: 'gray',
+	grey: 'grey'
+}
+
+export const GRADIENTS = {
+	lgbt: ['#750787', '#004dff', '#008026', '#ffed00', '#ff8c00', '#e40303'],
+	lgbtq: ['#750787', '#004dff', '#008026', '#ffed00', '#ff8c00', '#e40303'],
+	pride: ['#750787', '#004dff', '#008026', '#ffed00', '#ff8c00', '#e40303'],
+	agender: ['#000000', '#b9b9b9', '#ffffff', '#b8f483', '#ffffff', '#b9b9b9', '#000000'],
+	aromantic: ['#3da542', '#a7d379', '#ffffff', '#a9a9a9', '#000000'],
+	asexual: ['#000000', '#a3a3a3', '#ffffff', '#800080'],
+	bisexual: ['#d60270', '#d60270', '#9b4f96', '#0038a8', '#0038a8'],
+	genderfluid: ['#ff75a2', '#ffffff', '#be18d6', '#000000', '#333ebd'],
+	genderqueer: ['#b57edc', '#ffffff', '#4a8123'],
+	intersex: ['#ffd800', '#ffd800', '#7902aa', '#ffd800', '#ffd800'],
+	lesbian: ['#d52d00', '#ff9a56', '#ffffff', '#d362a4', '#a30262'],
+	nonbinary: ['#fcf434', '#ffffff', '#9c5cd4', '#2c2c2c'],
+	pansexual: ['#ff218c', '#ffd800', '#21b1ff'],
+	polysexual: ['#f61cb9', '#07d569', '#1c92f6'],
+	transgender: ['#5bcefa', '#f5a9b8', '#ffffff', '#f5a9b8', '#5bcefa']
+}
+
+export const ALIGNMENT = {
+  left: 'left',
+  center: 'center',
+  right: 'right',
+  top: 'top',
+  bottom: 'bottom'
+}
+
+export const FONTFACES = {
+	'console': 'console',
+	'block': 'block',
+	'simpleblock': 'simpleBlock',
+	'simple': 'simple',
+	'3d': '3d',
+	'simple3d': 'simple3d',
+	'chrome': 'chrome',
+	'huge': 'huge',
+	'shade': 'shade',
+	'slick': 'slick',
+	'grid': 'grid',
+	'pallet': 'pallet',
+	'tiny': 'tiny'
+}
+
+export const YT_DLP = 'yt-dlp'
+export const KWT = '/home/mango/Dev/yt-media-kit' // kit-working-directory
+export const PRD = '/home/mango/Dev/nexora' // production-directory
+export const MIN_RES = '32p'
+
+export const Rutas = {
+	assets: `${KWT}/storage/assets`,
+	audios: `${KWT}/storage/audios`,
+	audios_descargados: `${KWT}/storage/audios_descargados`,
+	completos: `${KWT}/storage/completos`,
+	info: `${KWT}/storage/info`,
+	videos: `${KWT}/storage/videos`,
+	videos_con_audio: `${KWT}/storage/videos_con_audio`,
+	videos_descargados: `${KWT}/storage/videos_descargados`,
+	list: `${KWT}/config/list.json`
+}
