89df55c069a118f74751daf3af09ec71c589f437
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-08-19 17:26:17 -0300

    feat: :sparkles: añadir función para crear todas las rutas necesarias para los videos

---
+++ src/cli/createDirectories.ts
@@ -0,0 +1,16 @@
+import { Rutas } from '../lib/constants'
+import { mkdirp } from 'fs-extra'
+import { writeFile } from 'node:fs/promises'
+
+export async function createDirectories (videoId: string) {
+  await mkdirp(Rutas.audios_descargados)
+  await mkdirp(Rutas.videos_descargados)
+  await mkdirp(Rutas.videos_con_audio)
+  await mkdirp(Rutas.videos)
+  await mkdirp(Rutas.audios)
+
+  await mkdirp(`${Rutas.completos}/${videoId}`)
+  await mkdirp(`${Rutas.assets}/${videoId}`)
+
+  await writeFile(`${Rutas.info}/${videoId}.json`, '')
+}
