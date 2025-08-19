6ecff830e7c832efa7a8ca2d441269b37b79186e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-08-19 16:58:27 -0300

    feat: :sparkles: crear función para añadir sugerencias a una dependencia

---
+++ src/patched/say.ts
@@ -0,0 +1,33 @@
+import { say as _say } from 'cfonts'
+import type { CFontsSettings, Colors } from '../env'
+import { COLORS } from '../lib/constants'
+
+const defaults: CFontsSettings = {
+  font: 'block',
+  align: 'left',
+  colors: [],
+  background: 'transparent',
+  letterSpacing: 1,
+  lineHeight: 1,
+  spaceless: false,
+  maxLength: 0,
+  gradient: false,
+  independentGradient: false,
+  transitionGradient: false,
+  rawMode: false,
+  env: 'node'
+}
+
+export function say (text: string, options: CFontsSettings) {
+  return _say(text, {
+    ...defaults,
+    ...options,
+    colors: parseColors(options.colors ?? [])
+  })
+}
+
+function parseColors (colors: (keyof Colors)[]) {
+  const arr: string[] = []
+  colors.forEach(color => arr.push(COLORS[color]))
+  return arr
+}
