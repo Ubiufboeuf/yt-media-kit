50a94cf30786efc93df1f6f3cb4a7a1d1758715e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-20 16:10:41 -0300

    refactor: cambiar textos que aparecen para preguntar la resolución

--- src/core/pipeline/steps/downloadVideo.ts
+++ src/core/pipeline/steps/downloadVideo.ts
@@ -12,16 +12,16 @@ import chalk from 'chalk'
 export async function askForResolution () {
   const res = await prompts({
     name: 'value',
-    message: 'Elige las resoluciones para descargar',
+    message: 'Elige las resoluciones del video',
     type: 'multiselect',
-    hint: 'Todas las opciones se descargan junto al audio',
+    hint: 'Todas las opciones se descargan sin audio',
     instructions: false,
     choices: [
       { title: 'Rendimiento (mínimo)', description: 'Usa la mínima resolución o descarga en 144p y lo deja en 32p', value: { download: '144p', desired: '32p', downloadNumber: 144, desiredNumber: 32 } },
-      { title: 'Música (bajo)', description: 'Descarga el video a 144p', value: { download: '144p', desired: '144p', downloadNumber: 144, desiredNumber: 144 } },
-      { title: 'Normal (medio)', description: 'Descarga el video a 360p', value: { download: '360p', desired: '360p', downloadNumber: 360, desiredNumber: 360 } },
-      { title: 'Leer (alto)', description: 'Descarga el video a 720p', value: { download: '720p', desired: '720p', downloadNumber: 720, desiredNumber: 720 } },
-      { title: 'Calidad (máximo)', description: 'Descarga el video a 1080p', value: { download: '1080p', desired: '1080p', downloadNumber: 1080, desiredNumber: 1080 } }
+      { title: 'Música (bajo)', description: 'Descarga el video en 144p', value: { download: '144p', desired: '144p', downloadNumber: 144, desiredNumber: 144 } },
+      { title: 'Normal (medio)', description: 'Descarga el video en 360p', value: { download: '360p', desired: '360p', downloadNumber: 360, desiredNumber: 360 } },
+      { title: 'Leer (alto)', description: 'Descarga el video en 720p', value: { download: '720p', desired: '720p', downloadNumber: 720, desiredNumber: 720 } },
+      { title: 'Calidad (máximo)', description: 'Descarga el video en 1080p', value: { download: '1080p', desired: '1080p', downloadNumber: 1080, desiredNumber: 1080 } }
     ],
     min: 1
   })
