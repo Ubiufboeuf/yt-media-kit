ba0f7122540f97f3f0488b84427c90c7bcf71e80
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-24 20:13:42 -0300

    feat: crear helper

--- src/core/helper.ts
+++ src/core/helper.ts
@@ -1,7 +1,22 @@
-import { say } from 'src/patched/say'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 export async function showHelper () {
-  say('HELP', {
-    colors: ['red', 'magenta']
-  })
+  const terminalWidth = Math.max(yargs(hideBin(process.argv)).terminalWidth() - 24, 48)
+  
+  const argv = yargs(hideBin(process.argv))
+    .usage('Usage: yt-media-kit [options] [url]')
+    .option('dev', {
+      describe: 'Ejecuta el programa en modo desarrollo. Esto omite cosas como la validación de los videos y activa otros parámetros forzadamente: --skip-validation y --use-default-video-id.'
+    })
+    .option('skip-validation', {
+      describe: 'Omite la validación del video.'
+    })
+    .option('use-default-video-id', {
+      describe: 'Usa un ID de video predefinido en vez de preguntar por él.'
+    })
+    .wrap(terminalWidth)
+    .parseSync()
+
+  console.log(argv)
 }
